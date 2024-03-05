0a2a924f6d86189e951e5979269a1dfe4182031d
samir <115088723+samirseraj03@users.noreply.github.com>
2024-03-05 15:56:00 +0100

    Agregando archivos grandes gestionados por Git LFS

---
+++ mapfont/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.mapfont',
+  appName: 'mapfont',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
