c9e15237c1ee4f5e81addc809a28621da5dfc89a
samir <115088723+samirseraj03@users.noreply.github.com>
2024-05-12 22:22:40 +0200

    optimizar cluster del mapa haciendo una query con el geojson

--- src/app/Fonts/Fonts.page.ts
+++ src/app/Fonts/Fonts.page.ts
@@ -84,81 +84,114 @@ export class fontsPage {
       .addTo(this.map);
   }
 
- 
-
   async getWatersourcesToMap() {
-
-  
+ 
 
     try {
       let geojson = await this.getStorageCache();
       if (!geojson) {
         // ponemos el storage
-        geojson = await this.setStorageIfnotExsit(geojson)
+        geojson = await this.setStorageIfnotExsit(geojson);
       }
       // Añade el GeoJSON al mapa de Mapbox
       this.map.on('load', () => {
+        const center_init = this.map.getCenter();
+        const zoom_init = this.map.getZoom();
+
+        const filterFeatures = (feature: any) => {
+          // Aquí definimos los criterios de filtrado
+          // Filtramos las características que están dentro de un área específica alrededor del centro inicial
+          // y tienen un zoom mayor o igual a 13
+          return (
+            this.map.getBounds().contains(feature.geometry.coordinates) &&
+            zoom_init >= 9
+          );
+        };
+        const filteredFeatures = geojson.features.filter(filterFeatures);
         // habilitamos el cluster y subimos el contendido
         this.map.addSource('watersources', {
           type: 'geojson',
-          data: geojson,
+          data: {
+            type: 'FeatureCollection',
+            features: filteredFeatures,
+          },
           cluster: true, // Habilita el clustering
           clusterMaxZoom: 14, // Zoom máximo para agrupar
           clusterRadius: 15, // Radio del cluster
         });
 
+
         this.map.on('moveend', () => {
           const center = this.map.getCenter();
           const zoom = this.map.getZoom();
-          
-          console.log('Nueva ubicación después del zoom:', center, 'Nivel de zoom:', zoom);
     
-          // Aquí puedes hacer lo que necesites con la nueva ubicación y el nivel de zoom
-          // Por ejemplo, puedes realizar una solicitud para obtener datos específicos basados en la nueva ubicación
-          const features = this.map.querySourceFeatures('watersources', {
-            filter: ['all', 
-            ['>=', zoom , 13], // Filtro para características que deben mostrarse solo a cierto nivel de zoom
-            ['within', [center.lng, center.lat ]] // Área visible del mapa
-          ] // Filtro para características que deben mostrarse solo a cierto nivel de zoom
-          });
-          
-          console.log(features)
+          // console.log(
+          //   'Nueva ubicación después del zoom:',
+          //   center,
+          //   'Nivel de zoom:',
+          //   zoom
+          // );
     
+          // Definir la función de filtrado
+          const filterFeatures = (feature: any) => {
+            // Aquí definimos los criterios de filtrado
+            // Filtramos las características que están dentro de un área específica alrededor del centro actual
+            // y tienen un zoom mayor o igual a 13
+            return (
+              this.map.getBounds().contains(feature.geometry.coordinates) &&
+              zoom >= 9
+            );
+          };
+    
+          // Filtrar el GeoJSON según la función de filtrado
+          const filteredFeatures = geojson.features.filter(filterFeatures);
+
+          // Obtener la fuente existente
+          const existingSource = this.map.getSource('watersources');
+
+          // Verificar si la fuente ya existe y si hay capas asociadas a ella
+          if (existingSource) {
+              // Actualizar los datos de la fuente existente
+              existingSource.setData({
+                  type: 'FeatureCollection',
+                  features: filteredFeatures
+              });
+          } else {
+              console.error('La fuente "watersources" no existe en el mapa.');
+          }
         });
 
-
-    
         // Añade una capa de puntos para representar las fuentes de agua en el mapa
         this.map.addLayer({
-            id: 'clusters',
-            type: 'circle',
-            source: 'watersources',
-            filter: ['has', 'point_count'],
-            paint: {
-                // Use step expressions (https://docs.mapbox.com/style-spec/reference/expressions/#step)
-                // with three steps to implement three types of circles:
-                //   * Blue, 20px circles when point count is less than 100
-                //   * Yellow, 30px circles when point count is between 100 and 750
-                //   * Pink, 40px circles when point count is greater than or equal to 750
-                'circle-color': [
-                    'step',
-                    ['get', 'point_count'],
-                    '#51bbd6',
-                    100,
-                    '#f1f075',
-                    750,
-                    '#f28cb1'
-                ],
-                'circle-radius': [
-                    'step',
-                    ['get', 'point_count'],
-                    20,
-                    100,
-                    30,
-                    750,
-                    40
-                ]
-            }
+          id: 'clusters',
+          type: 'circle',
+          source: 'watersources',
+          filter: ['has', 'point_count'],
+          paint: {
+            // Use step expressions (https://docs.mapbox.com/style-spec/reference/expressions/#step)
+            // with three steps to implement three types of circles:
+            //   * Blue, 20px circles when point count is less than 100
+            //   * Yellow, 30px circles when point count is between 100 and 750
+            //   * Pink, 40px circles when point count is greater than or equal to 750
+            'circle-color': [
+              'step',
+              ['get', 'point_count'],
+              '#51bbd6',
+              100,
+              '#f1f075',
+              750,
+              '#f28cb1',
+            ],
+            'circle-radius': [
+              'step',
+              ['get', 'point_count'],
+              20,
+              100,
+              30,
+              750,
+              40,
+            ],
+          },
         });
 
         this.map.addLayer({
@@ -186,43 +219,39 @@ export class fontsPage {
           },
         });
 
-        
-        this.map.on('click', 'clusters', (e : any) => {
+        this.map.on('click', 'clusters', (e: any) => {
           const features = this.map.queryRenderedFeatures(e.point, {
-              layers: ['clusters']
+            layers: ['clusters'],
           });
           const clusterId = features[0].properties.cluster_id;
-          this.map.getSource('watersources').getClusterExpansionZoom(
-              clusterId,
-              (err: any, zoom : any) => {
-                  if (err) return;
+          this.map
+            .getSource('watersources')
+            .getClusterExpansionZoom(clusterId, (err: any, zoom: any) => {
+              if (err) return;
 
-                  this.map.easeTo({
-                      center: features[0].geometry.coordinates,
-                      zoom: zoom
-                  });
-              }
-          );
-      });
-      
-      
+              this.map.easeTo({
+                center: features[0].geometry.coordinates,
+                zoom: zoom,
+              });
+            });
+        });
+
         // When a click event occurs on a feature in
         // the unclustered-point layer, open a popup at
         // the location of the feature, with
         // description HTML from its properties.
-        this.map.on('click', 'unclustered-point', async (e : any) => {
+        this.map.on('click', 'unclustered-point', async (e: any) => {
           const coordinates = e.features[0].geometry.coordinates.slice();
           const feature = e.features[0];
           const properties = feature.properties;
-
 
           // Ensure that if the map is zoomed out such that
           // multiple copies of the feature are visible, the
           // popup appears over the copy being pointed to.
           while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-              coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+            coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
           }
-          let popup : any
+          let popup: any;
           popup = await this.UploadPopup(
             properties.id,
             properties.name,
@@ -236,41 +265,36 @@ export class fontsPage {
             coordinates
           );
 
-            // esperamos que se crea el doom para cambiar los estilos,
-            // no lo cambiamos antes porque queremos cambiar el cierre y el color.
+          // esperamos que se crea el doom para cambiar los estilos,
+          // no lo cambiamos antes porque queremos cambiar el cierre y el color.
 
-            const popupContent = popup.getElement(); // Obtener el elemento del contenido del popup
+          const popupContent = popup.getElement(); // Obtener el elemento del contenido del popup
 
-            popup.getElement().style.color = '#000000'; // Set text color to black for text popup
+          popup.getElement().style.color = '#000000'; // Set text color to black for text popup
 
-            const closeButton = popupContent.querySelector(
-              '.mapboxgl-popup-close-button'
-            ); // Obtener el botón de cierre
-            // Aplicar estilos al botón de cierre
-            if (closeButton instanceof HTMLElement) {
-              closeButton.style.color = '#ff0000'; // Cambiar color del texto del botón
-              closeButton.style.background = '#ffffff'; // Cambiar color de fondo del botón
-              closeButton.style.width = '20px';
-              // Aquí puedes agregar más estilos según tus necesidades
-            }
+          const closeButton = popupContent.querySelector(
+            '.mapboxgl-popup-close-button'
+          ); // Obtener el botón de cierre
+          // Aplicar estilos al botón de cierre
+          if (closeButton instanceof HTMLElement) {
+            closeButton.style.color = '#ff0000'; // Cambiar color del texto del botón
+            closeButton.style.background = '#ffffff'; // Cambiar color de fondo del botón
+            closeButton.style.width = '20px';
+            // Aquí puedes agregar más estilos según tus necesidades
+          }
+        });
 
-      });
-
-      this.map.on('mouseenter', 'clusters', () => {
-        this.map.getCanvas().style.cursor = 'pointer';
-      });
-      this.map.on('mouseleave', 'clusters', () => {
-        this.map.getCanvas().style.cursor = '';
-      });
- 
-
-       
+        this.map.on('mouseenter', 'clusters', () => {
+          this.map.getCanvas().style.cursor = 'pointer';
+        });
+        this.map.on('mouseleave', 'clusters', () => {
+          this.map.getCanvas().style.cursor = '';
+        });
       });
     } catch (error) {
       console.log(error);
     }
   }
-
 
   async UploadPopup(
     id: any,
@@ -282,25 +306,24 @@ export class fontsPage {
     lng: any,
     photo: string,
     address: string,
-    coordinates : any
+    coordinates: any
   ) {
     // buscamos si hay foto o no
     let photo_url = photo
       ? this.Supabase.GetStorage(photo)
       : './assets/icon/agua-potable.png';
 
-
     // miramos si esta disponible la fuente:
     const availableText = available ? 'Disponible' : 'No disponible';
     const isPotableText = ispotable ? 'Potable' : 'No potable';
     // create the popup
     const popup = new mapboxgl.Popup({
-      
       offset: 25,
       className: 'MarkersMapBox',
     })
-    .setLngLat(coordinates)
-    .setHTML(`
+      .setLngLat(coordinates)
+      .setHTML(
+        `
       <p class="mt-3 mb-0"><strong>Nombre de la fuente:</strong> ${name}</p>
       <p class="mb-0" ><strong>Información de la fuente:</strong></p>
       <ul>
@@ -347,65 +370,58 @@ export class fontsPage {
         </div>
       </ion-content>
     </ion-modal>
-  `)
-  .addTo(this.map);
-    return popup
+  `
+      )
+      .addTo(this.map);
+    return popup;
   }
 
+  // chkeamos si hay cahche y devolcemos el geojson
+  async setStorageIfnotExsit(geojson: any) {
+    // Llama a Supabase para obtener los datos de las fuentes de agua
+    let watersources = await this.Supabase.getWaterSources();
+    console.log(watersources);
 
-
-
-
- // chkeamos si hay cahche y devolcemos el geojson
- async setStorageIfnotExsit(geojson: any) {
-  // Llama a Supabase para obtener los datos de las fuentes de agua
-  let watersources = await this.Supabase.getWaterSources();
-  console.log(watersources);
-
-  if (Array.isArray(watersources) && watersources.length > 0) {
-    // Crea un objeto GeoJSON
-    geojson = {
-      type: 'FeatureCollection',
-      features: [],
-    };
-    // Itera sobre los datos de las fuentes de agua y añade cada punto al GeoJSON
-    watersources.forEach((element) => {
-      const feature = {
-        type: 'Feature',
-        geometry: {
-          type: 'Point',
-          coordinates: [
-            element.location.longitude,
-            element.location.latitude,
-          ],
-        },
-        properties: {
-          id: element.id,
-          name: element.name,
-          available: element.available,
-          description: element.description,
-          ispotable: element.ispotable,
-          photo: element.photo,
-          address: element.address,
-        },
+    if (Array.isArray(watersources) && watersources.length > 0) {
+      // Crea un objeto GeoJSON
+      geojson = {
+        type: 'FeatureCollection',
+        features: [],
       };
-      geojson.features.push(feature);
-    });
+      // Itera sobre los datos de las fuentes de agua y añade cada punto al GeoJSON
+      watersources.forEach((element) => {
+        const feature = {
+          type: 'Feature',
+          geometry: {
+            type: 'Point',
+            coordinates: [
+              element.location.longitude,
+              element.location.latitude,
+            ],
+          },
+          properties: {
+            id: element.id,
+            name: element.name,
+            available: element.available,
+            description: element.description,
+            ispotable: element.ispotable,
+            photo: element.photo,
+            address: element.address,
+          },
+        };
+        geojson.features.push(feature);
+      });
 
-    this.setStorageCache(geojson);
+      this.setStorageCache(geojson);
+    }
+    return geojson;
   }
-  return geojson
-}
-
-  
 
   // cargamos los scripts a los marcadores
   cargarScript(): void {
     document.addEventListener('DOMContentLoaded', () => {
       var modal = document.querySelector('ion-modal');
     });
-
-  
 
     (window as any).OnNavigate = async (lng: any, lat: any) => {
       let link = await this.GeolocationService.generateGoogleMapsLink(lat, lng);
